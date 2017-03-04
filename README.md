a45179ebed18a56287b0eb8ca645bac073e12f61
Krista <krista.fryauff@gmail.com>
2017-03-04 23:21:11 +0000

    adding c9 pages

---
+++ README.md
@@ -0,0 +1,13 @@
+# Tetris:
+
+## Running
+
+1) Launch `server.js` and start the app from the Terminal:
+
+    $ node server.js
+
+
+
+
+--
+Built with Cloud9 IDE
---
+++ client/js/app.js
@@ -0,0 +1,12 @@
+/*global import export */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return (
+        <div>APP</div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
